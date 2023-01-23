94805ad5abfb30f24e51cd87eb6fb714f29a3a0c
Marcos Araoz <araozmarcos11@gmail.com>
2023-01-23 19:01:40 -0300

    nueva__entrega

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-let producto = parseInt(prompt(`Selecciona el producto que deseas comprar: 1.Zapatillas - 2.Campera - 3.Remera`))
 
- let totalCompra = 0
- let seguirComprando = true
- let decision
+let producto = parseInt(prompt(
+      "Selecciona el producto que deseas comprar: 1.Zapatillas - 2.Campera - 3.Remera"
+    )
+  )
+  let totalCompra = 0
+  let seguirComprando = true
+  let decision
+  const productosExistentes = [] 
 
-while (seguirComprando === true) {
-     if (producto === 1) {
-        totalCompra = totalCompra + 35000
-     }else if (producto === 2){
-        totalCompra = totalCompra + 11000
-     }else if (producto === 3){
-        totalCompra = totalCompra + 8000
-     }else{
-        producto = parseInt(prompt(`Selecciona un producto habilitado:1.Zapatillas - 2.Campera - 3.Remera`))
-        continue
-     }
-
-  decision = parseInt(prompt(`Quieres seguir comprando ? 1.Si - 2.No `))
-  if(decision === 1){
-     producto = parseInt(prompt(`Selecciona el producto que deseas comprar: 1.Zapatillas - 2.Campera - 3.Remera`))
-  }else{
-    seguirComprando = false
+  class Producto{
+    constructor(id,nombre,precio){
+      this.id = id
+      this.nombre = nombre
+      this.precio = precio
+    }
   }
-}
-alert (`El total de tu compra es ${totalCompra}`)
+    const zapatillas = new Producto (1,`Zapatillas`,35000)
+    productosExistentes.push(zapatillas)
+    const campera = new Producto (2,`Campera`,10000)
+    productosExistentes.push(campera)
+    const remera = new Producto (3,`Remera`,8000)
+    productosExistentes.push(remera)
+    
+
+    console.log(productosExistentes);
+
+  while (seguirComprando === true) {
+    if (producto == 1) {
+      totalCompra = totalCompra + zapatillas.precio
+    } else if (producto === 2) {
+      totalCompra = totalCompra + campera.precio
+    } else if (producto === 3) {
+      totalCompra = totalCompra + remera.precio
+    } else {
+      producto = parseInt(prompt("Selecciona un producto habilitado: 1.Zapatillas - 2.Campera - 3.Remera "))
+      continue
+  }
+    decision = parseInt(prompt('Quieres seguir comprando? 1.Si - 2.No'))
+    if (decision === 1) {
+      producto = parseInt(
+        prompt(
+          "Selecciona el producto que deseas comprar: 1.Zapatillas - 2.Campera - 3.Remera"
+        )
+      )
+    } else {
+      seguirComprando = false
+    }
+  }
+  alert("El total de tu compra es " + totalCompra)
